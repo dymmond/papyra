e5e454adf472fde84750020b4032a2d1e51fa627
tarsil <tiago.arasilva@gmail.com>
2026-01-09 14:12:06 +0000

    Add contract and contract tests

--- papyra/persistence/__init__.py
+++ papyra/persistence/__init__.py
@@ -1,7 +1,17 @@
+from .contract import (
+    PersistenceBackendCapabilities,
+    PersistenceBackendContract,
+    backend_capabilities,
+    safe_metrics_snapshot,
+)
 from .json import JsonFilePersistence
 from .memory import InMemoryPersistence
 
 __all__ = [
     "InMemoryPersistence",
     "JsonFilePersistence",
+    "PersistenceBackendCapabilities",
+    "PersistenceBackendContract",
+    "backend_capabilities",
+    "safe_metrics_snapshot",
 ]
